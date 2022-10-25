ddf2c971c02d2230f1ed60334a588eeff12f8c25
Jonathan <71567979+moseschgit@users.noreply.github.com>
2022-10-25 09:12:31 +0000

    make print x

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+<<<<<<< HEAD
 #Themen:
 #Team: Lauremz, Vanessa, Jonathan
 
 x = 1+1 
 print(x)
+=======
+Themen:
+Team: Lauremz, Vanessa, Jonathan
+  
+hello test
+>>>>>>> origin/main
+
+
+x = 1+1 
+ print(x)
