2d83849eb7175712e8f8875c77997c16d6437907
Jonathan <71567979+moseschgit@users.noreply.github.com>
2022-11-08 09:36:00 +0000

    1. try to start with a code

---
+++ main.py
@@ -0,0 +1,21 @@
+#IMP Projekt: Bildkomprimierung 
+#Gruppe: Laurenz Jonathan Vanessa
+#https://www.youtube.com/watch?v=wI9yE7ZIbMg
+
+
+#import PIL 
+#from PIL import Image
+
+#Anfang = int(input("Write 10, in order to start the picture compression."))
+
+#def save_compressed():
+#  img.save("./images/IMP Bild Lichtenstern Logo.jpg","JPEG", optimize=True, quality=100)
+
+
+
+
+  
+
+#if Anfang == 10:
+#  img = Image.open("./images/IMP Bild Lichtenstern Logo.png")
+
