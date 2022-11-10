4fb30b45eb41fdfe14df0fb29953f9f256bfad26
Jonathan <71567979+moseschgit@users.noreply.github.com>
2022-11-10 14:28:38 +0000

    Überlegungen fürs Projekt 2.0

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 #IMP Projekt: Bildkomprimierung 
 #Gruppe: Laurenz Jonathan Vanessa
-#https://www.youtube.com/watch?v=wI9yE7ZIbMg
+
+
+
+#Protokoll noch schreiben
+# schauen, was wir sonst noch machen müssen vanessa
+#codierung verstehen, erklären ycbcr laurenz
+#ideen holen, nachdenken wie man es programmieren könnte jonathan
+#protokoll schreiben : Jonathan
+#erklärung/ präsentation zum Projekt: Vanessa (schaut was wir bei der präsentation machen könnten, z.B. erklärung komprimierung generell, ycbcr generell usw.)
+#--> alles visualisieren und fortschritte, sowie notizen in gemeinsamme gruppe schicken
+
 
 #Überlegung 1 
 #Bild in Replit einfügen --> Überlegung 2 wird durchgeführt --> Bild wird komprimiert Ausgegeben
@@ -13,9 +23,6 @@
 #pixel werden mit Listen angegeben, die zusammengefasst werden 
 #wenn pixel, die nebeneinander liegen ähnlich sind, werden sie zusammengefasst.
 #am Ende schauen, wie viel gespeichert wurde
-
-
-
 
 
 
