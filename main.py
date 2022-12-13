66ecb652f99128f0cd6dd33f84f2e574e484c0e6
Jonathan <71567979+moseschgit@users.noreply.github.com>
2022-12-13 10:27:39 +0000

    RGB wird definiert

--- main.py
+++ main.py
@@ -47,24 +47,74 @@
 
 #Jedes Bild wird in 16 Raster aufgeteilt (4x4) bei jedem Raster bestimmte Anzahl an Werten (hier im bsp. 8 Werte) --> NOCH SCHAUEN WIE MAN DIE EINZELNEN FARBEN IN ZAHLEN DEFINIERT
 
-a1= 100
-b2= 200
-c3= 155
-d4= 160
-e5= 170
-f6= 180
-g7= 190
-h8= 230
+#a1= 100
+#b2= 200
+#c3= 155
+#d4= 160
+#e5= 170
+#f6= 180
+#g7= 190
+#h8= 200
 
 #Listen werden mit Variablen aufgebaut 
 
+
+
+
+
+
+
 #l1 = [a1, b2, c3, d4, e5, f6,g7, h8]
+
+
+
 
 #Mittelwert wird ausgerechnet 
 
-pythonic_machine_ages = [a1, b2, c3, d4, e5, f6,g7, h8]
+#pythonic_machine_ages = [a1, b2, c3, d4, e5, f6,g7, h8]
 
-def mean(dataset):
-    return sum(dataset) / len(dataset)
+#def mean(dataset):
+#    return sum(dataset) / len(dataset)
 
-print(mean(pythonic_machine_ages))
+
+
+
+#print(mean(pythonic_machine_ages))
+
+#def pixelabfrage(rgb_im, x, y):
+# rgb_im = rgb_im
+#  x = x
+#  y = y
+#  r, g, b = rgb_im.getpixel ((x,y))
+
+#print ('Pixelkoordinaten:%3s%3s  rot:%3s grün:3%s  blau:%3s'% (x, y, r, g, b))
+
+#rgb in ycbcr umwandeln dann graustufe in liste eintragen und mittelwert
+
+
+
+
+#Pixel definieren
+#p=Pixel x=Zahl des Pixels
+
+#px
+#pixel bestehen aus r , g , b
+
+
+#p3, p6, p9, p16:
+
+r36916 = 255
+g36916 = 0
+b36916 = 0
+
+#p2, p4, p5, p7, p10, p12, p13, p15:
+
+r = 0
+g = 255
+b = 0
+
+#p1, p8, p11, p14:
+
+r = 0
+g = 0
+b = 255
